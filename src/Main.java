b6c43372656f29d4ce005189263d5e72cbccf4c2
xw <2717539837@qq.com>
2022-12-27 20:19:39 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("hello git5");
+        System.out.println("hello git6");
     }
 }
