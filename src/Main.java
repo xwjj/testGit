c564cf1644608a6083442b7826aaa8c8a15be65a
xw <2717539837@qq.com>
2022-12-27 20:15:27 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+    }
+}
